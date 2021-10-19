bff500094260073a0897fed903a77248cfc3b5c1
tikkasandy <tikkasandy@gmail.com>
2021-10-19 17:38:31 +0300

    Make task

--- src/index.js
+++ src/index.js
@@ -1,52 +1,56 @@
-class CountdownTimer {
-    constructor({ selector, targetDate }) {
-        this.selector = selector;
-        this.targetDate = targetDate;
-        this.refs = this.getRefs(this.selector);
-        this.start();
-    }
+import debounce from "lodash.debounce"
 
-    start() {
-        let timerValue = document.querySelectorAll(`${this.selector} .value`);
-        setInterval(() => {
-            const currentTime = Date.now();
-            const deltaTime = this.targetDate - currentTime;
-            this.updateClockface(this.getTimeComponents(deltaTime));
-        }, 1000);
-    }
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import { error } from '@pnotify/core';
 
-    getTimeComponents(time) {
-        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-        const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-        return { days, hours, mins, secs };
-    }
+import './sass/main.scss';
 
-    pad(value) {
-        return String(value).padStart(2, '0');
+import countryList from "./templates/countries-list.hbs"
+import countryCard from "./templates/country-card.hbs"
+
+import refs from "./js/refs"
+import fetchCountries from "./js/fetchCountries";
+
+
+refs.countrySearch.addEventListener("input", debounce(onSearch, 500));
+
+function onSearch(e) {
+    const countryName = e.target.value.trim();
+    if (countryName.length > 0) {
+        fetchCountries(countryName)
+            .then(renderCountry)
+            .catch(onFetchError);
+    } else {
+        clear()
     };
+};
 
-    updateClockface({ days, hours, mins, secs }) {
-        this.refs.days.textContent = days;
-        this.refs.hours.textContent = hours;
-        this.refs.mins.textContent = mins;
-        this.refs.secs.textContent = secs;
-    }
-
-    getRefs(selector) {
-        const ourTimer = document.querySelector(selector);
-        const refs = {
-            days: ourTimer.querySelector('[data-value = "days"]'),
-            hours: ourTimer.querySelector('[data-value = "hours"]'),
-            mins: ourTimer.querySelector('[data-value = "mins"]'),
-            secs: ourTimer.querySelector('[data-value = "secs"]'),
-        };
-        return refs;
+function renderCountry(country) {
+    if (country.status === 404) {
+        clear();
+        onFetchError("Country not found. Please enter a correct query!");
+    } else {
+        if (country.length === 1) {
+            refs.resultContainer.innerHTML = countryCard(country);
+        } else {
+            if (country.length < 11) {
+                refs.resultContainer.innerHTML = countryList(country);
+            } else {
+                clear();
+                onFetchError("Too many matches found. Please enter a more specific query!");
+            }
+        }
     }
 }
 
-const myBirthdayTimer = new CountdownTimer({
-    selector: '#timer-1',
-    targetDate: new Date('Apr 05, 2022'),
-});
+function onFetchError(message) {
+    error({
+        text: `${message}`,
+        delay: 2500,
+    });
+}
+
+function clear() {
+    refs.resultContainer.innerHTML = "";
+}
